9745736743b3fc9d4d962ba284e39a39165dbc6c
RaLtz91 <163421676+RaLtz91@users.noreply.github.com>
2025-03-06 08:34:08 -0300

    botão +

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded',function(){
+ const aumentaFonteBotao = document.getElementById('aumenta-fonte');
+ let tamanhoAtualFonte = 1;
+  aumentaFonteBotao.addEventListener('click',function(){
+    tamanhoAtualFonte += 0,1;
+    document.body.style.fontSize = '$(tamanhoAtualFonte)'
+  })
+ })
